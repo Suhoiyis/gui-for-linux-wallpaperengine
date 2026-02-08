9010e8866080c338a82781ee1fde52a3d730c264
Suhoisy@arch-yu <3218818428@qq.com>
2026-02-08 22:23:30 +0800

    style(ui): overhaul sidebar layout and unify metadata alignment

--- py_GUI/ui/components/sidebar.py
+++ py_GUI/ui/components/sidebar.py
@@ -5,7 +5,7 @@ import gi
 gi.require_version('Gtk', '4.0')
 gi.require_version('Gdk', '4.0')
 gi.require_version('Adw', '1')
-from gi.repository import Gtk, Gdk, GLib, Adw, GdkPixbuf
+from gi.repository import Gtk, Gdk, GLib, Adw, GdkPixbuf, Pango
 
 from py_GUI.core.wallpaper import WallpaperManager
 from py_GUI.core.properties import PropertiesManager
@@ -31,8 +31,9 @@ class Sidebar(Gtk.Box):
         self.get_apply_mode: Callable[[], str] = lambda: "diff"
         
         self.add_css_class("sidebar")
-        self.set_size_request(320, -1)
+        self.set_size_request(370, -1)
         self.set_hexpand(False)
+        self.set_halign(Gtk.Align.END)
         
         self._compact_mode = False
         
@@ -99,7 +100,7 @@ class Sidebar(Gtk.Box):
 
         content = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         content.set_hexpand(False)
-        content.set_size_request(320, -1)
+        content.set_size_request(370, -1)
         scroll.set_child(content)
 
         # Preview Image
@@ -119,36 +120,21 @@ class Sidebar(Gtk.Box):
         preview_scroll.set_child(self.preview_image)
         content.append(preview_container)
 
-        # Title Row (Title + spacer + Edit Button)
-        title_row = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=8)
-        title_row.set_halign(Gtk.Align.FILL)
-        title_row.set_margin_start(20)
-        title_row.set_margin_end(20)
-        content.append(title_row)
+        # Title Section
+        title_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        title_box.set_margin_start(20)
+        title_box.set_margin_end(20)
+        content.append(title_box)
 
         self.lbl_title = Gtk.Label(label="Select a Wallpaper")
         self.lbl_title.add_css_class("sidebar-title")
         self.lbl_title.set_use_markup(True)
         self.lbl_title.set_halign(Gtk.Align.START)
         self.lbl_title.set_wrap(True)
-        self.lbl_title.set_max_width_chars(20)
+        self.lbl_title.set_max_width_chars(25)
         self.lbl_title.set_xalign(0)
-        title_row.append(self.lbl_title)
+        title_box.append(self.lbl_title)
 
-        spacer = Gtk.Box()
-        spacer.set_hexpand(True)
-        title_row.append(spacer)
-
-        self.btn_edit_nickname = Gtk.Button()
-        self.btn_edit_nickname.set_icon_name("document-edit-symbolic")
-        self.btn_edit_nickname.add_css_class("flat")
-        self.btn_edit_nickname.add_css_class("circular")
-        self.btn_edit_nickname.set_tooltip_text("Edit Nickname")
-        self.btn_edit_nickname.set_valign(Gtk.Align.CENTER)
-        self.btn_edit_nickname.set_visible(False)
-        title_row.append(self.btn_edit_nickname)
-
-        # Original Name Label (below title)
         self.lbl_original_name = Gtk.Label(label="")
         self.lbl_original_name.add_css_class("original-name-text")
         self.lbl_original_name.set_halign(Gtk.Align.START)
@@ -156,16 +142,21 @@ class Sidebar(Gtk.Box):
         self.lbl_original_name.set_max_width_chars(30)
         self.lbl_original_name.set_xalign(0)
         self.lbl_original_name.set_visible(False)
-        content.append(self.lbl_original_name)
+        title_box.append(self.lbl_original_name)
 
+        # Info Row (Chips + Edit Button)
         folder_row = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=8)
-        folder_row.set_halign(Gtk.Align.START)
+        folder_row.set_halign(Gtk.Align.FILL)
+        folder_row.set_margin_start(20)
+        folder_row.set_margin_end(20)
         content.append(folder_row)
 
         self.lbl_folder = Gtk.Label(label="")
         self.lbl_folder.add_css_class("folder-chip")
         self.lbl_folder.set_tooltip_text("Click to copy ID")
         self.lbl_folder.set_cursor_from_name("pointer")
+        self.lbl_folder.set_ellipsize(Pango.EllipsizeMode.END)
+        self.lbl_folder.set_max_width_chars(12)
         folder_row.append(self.lbl_folder)
 
         self.lbl_size = Gtk.Label(label="")
@@ -176,6 +167,20 @@ class Sidebar(Gtk.Box):
         self.lbl_index.add_css_class("index-chip")
         folder_row.append(self.lbl_index)
 
+        # Spacer to push button to far right
+        row_spacer = Gtk.Box()
+        row_spacer.set_hexpand(True)
+        folder_row.append(row_spacer)
+
+        self.btn_edit_nickname = Gtk.Button()
+        self.btn_edit_nickname.set_icon_name("document-edit-symbolic")
+        self.btn_edit_nickname.add_css_class("flat")
+        self.btn_edit_nickname.add_css_class("circular")
+        self.btn_edit_nickname.set_tooltip_text("Edit Nickname")
+        self.btn_edit_nickname.set_valign(Gtk.Align.CENTER)
+        self.btn_edit_nickname.set_visible(False)
+        folder_row.append(self.btn_edit_nickname)
+
         # Folder click to copy
         folder_click = Gtk.GestureClick.new()
         folder_click.connect("released", self.on_folder_clicked)
@@ -185,10 +190,13 @@ class Sidebar(Gtk.Box):
         self.type_header = Gtk.Label(label="Type")
         self.type_header.add_css_class("sidebar-section")
         self.type_header.set_halign(Gtk.Align.START)
+        self.type_header.set_margin_start(20)
+        self.type_header.set_margin_end(20)
         content.append(self.type_header)
 
         self.type_container = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         self.type_container.set_margin_start(20)
+        self.type_container.set_margin_end(20)
         content.append(self.type_container)
 
         self.lbl_type = Gtk.Label(label="-")
@@ -200,22 +208,30 @@ class Sidebar(Gtk.Box):
         self.tags_header = Gtk.Label(label="Tags")
         self.tags_header.add_css_class("sidebar-section")
         self.tags_header.set_halign(Gtk.Align.START)
+        self.tags_header.set_margin_start(20)
+        self.tags_header.set_margin_end(20)
         content.append(self.tags_header)
 
         self.tags_container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         self.tags_container.set_hexpand(False)
+        self.tags_container.set_margin_start(20)
+        self.tags_container.set_margin_end(20)
         content.append(self.tags_container)
 
         self.tags_flow = Gtk.FlowBox()
         self.tags_flow.set_selection_mode(Gtk.SelectionMode.NONE)
         self.tags_flow.set_max_children_per_line(4)
         self.tags_flow.set_hexpand(False)
+        self.tags_flow.set_column_spacing(4)
+        self.tags_flow.set_row_spacing(4)
         self.tags_container.append(self.tags_flow)
 
         # Description
         self.desc_header = Gtk.Label(label="Description")
         self.desc_header.add_css_class("sidebar-section")
         self.desc_header.set_halign(Gtk.Align.START)
+        self.desc_header.set_margin_start(20)
+        self.desc_header.set_margin_end(20)
         content.append(self.desc_header)
 
         self.lbl_desc = Gtk.Label(label="No description.")
@@ -226,28 +242,9 @@ class Sidebar(Gtk.Box):
         self.lbl_desc.set_use_markup(True)
         self.lbl_desc.set_max_width_chars(30)
         self.lbl_desc.set_selectable(True)
+        self.lbl_desc.set_margin_start(20)
+        self.lbl_desc.set_margin_end(20)
         content.append(self.lbl_desc)
-
-        # Properties
-        # sep = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL)
-        # sep.set_margin_top(20)
-        # content.append(sep)
-
-        # props_header = Gtk.Label(label="Properties")
-        # props_header.add_css_class("sidebar-section")
-        # props_header.set_margin_top(15)
-        # props_header.set_halign(Gtk.Align.START)
-        # content.append(props_header)
-
-        # self.props_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        # self.props_box.set_margin_start(15)
-        # self.props_box.set_margin_end(15)
-        # self.props_box.set_hexpand(False)
-        # content.append(self.props_box)
-
-        # self.props_loading_lbl = Gtk.Label(label="Loading properties...")
-        # self.props_loading_lbl.add_css_class("text-muted")
-        # Initially not added
 
         # Bottom Buttons
         btn_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
@@ -262,12 +259,6 @@ class Sidebar(Gtk.Box):
         
         # Initial button setup
         self.update_apply_button_state()
-
-        # self.btn_stop = Gtk.Button(label="Stop Wallpaper")
-        # self.btn_stop.add_css_class("sidebar-btn")
-        # self.btn_stop.add_css_class("danger")
-        # self.btn_stop.connect("clicked", self.on_stop_clicked)
-        # btn_box.append(self.btn_stop)
 
         self.btn_workshop = Gtk.Button(label="Open in Workshop")
         self.btn_workshop.add_css_class("sidebar-btn")
@@ -452,166 +443,6 @@ class Sidebar(Gtk.Box):
             child = self.tags_flow.get_first_child()
             if child is None: break
             self.tags_flow.remove(child)
-
-        # Clear properties
-        # while True:
-        #     child = self.props_box.get_first_child()
-        #     if child is None: break
-        #     self.props_box.remove(child)
-
-    # def load_properties(self, wp_id: str):
-    #     # Clear existing
-    #     while True:
-    #         child = self.props_box.get_first_child()
-    #         if child is None: break
-    #         self.props_box.remove(child)
-
-    #     self.props_box.append(self.props_loading_lbl)
-
-    #     def load_async():
-    #         properties = self.prop_manager.get_properties(wp_id)
-    #         GLib.idle_add(lambda: self.display_properties(properties))
-
-    #     threading.Thread(target=load_async, daemon=True).start()
-
-    # def display_properties(self, properties: List[Dict]):
-    #     if self.props_loading_lbl.get_parent():
-    #         self.props_box.remove(self.props_loading_lbl)
-
-    #     if not properties:
-    #         no_props = Gtk.Label(label="No properties available.")
-    #         no_props.add_css_class("text-muted")
-    #         self.props_box.append(no_props)
-    #         return
-
-    #     for prop in properties:
-    #         widget = self.create_property_widget(prop)
-    #         self.props_box.append(widget)
-
-    # def create_property_widget(self, prop: Dict) -> Gtk.Widget:
-    #     container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-    #     container.set_margin_top(8)
-    #     container.set_hexpand(False)
-
-    #     title_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-    #     title_box.set_hexpand(False)
-    #     container.append(title_box)
-
-    #     title_label = Gtk.Label(label=prop.get('text', prop['name']))
-    #     title_label.set_halign(Gtk.Align.START)
-    #     title_label.add_css_class("setting-label")
-    #     title_box.append(title_label)
-
-    #     prop_type = prop['type']
-    #     prop_name = prop['name']
-    #     
-    #     if not self.selected_wp: return
-    #     user_value = self.prop_manager.get_user_property(self.selected_wp, prop_name)
-    #     current_value = user_value if user_value is not None else prop['value']
-
-    #     if prop_type == 'boolean':
-    #         switch = Gtk.Switch()
-    #         switch.set_active(bool(current_value))
-    #         switch.set_valign(Gtk.Align.CENTER)
-    #         switch.set_halign(Gtk.Align.START)
-    #         switch.set_hexpand(False)
-    #         switch.connect('state-set', lambda s, v: self.on_property_changed(prop_name, v, 'boolean'))
-    #         container.append(switch)
-
-    #     elif prop_type == 'slider':
-    #         slider = Gtk.Scale.new_with_range(Gtk.Orientation.HORIZONTAL, prop['min'], prop['max'], prop['step'])
-    #         slider.set_value(float(current_value or 0))
-    #         slider.set_hexpand(False)
-    #         slider.set_size_request(280, -1)
-    #         
-    #         val_lbl = Gtk.Label(label=f"{float(current_value or 0):.2f}")
-    #         val_lbl.add_css_class("text-muted")
-    #         val_lbl.set_halign(Gtk.Align.START)
-    #         
-    #         def on_slider_changed(s):
-    #             val = s.get_value()
-    #             val_lbl.set_label(f"{val:.2f}")
-    #             self.on_property_changed(prop_name, val, 'slider')
-    #             
-    #         slider.connect('value-changed', on_slider_changed)
-    #         container.append(slider)
-    #         container.append(val_lbl)
-
-    #     elif prop_type == 'color':
-    #         color = Gtk.ColorButton()
-    #         color.set_hexpand(False)
-    #         if isinstance(current_value, (tuple, list)) and len(current_value) >= 3:
-    #             gdk_color = Gdk.RGBA()
-    #             gdk_color.parse(f"rgb({int(current_value[0]*255)}, {int(current_value[1]*255)}, {int(current_value[2]*255)})")
-    #             color.set_rgba(gdk_color)
-    #         color.connect('color-set', lambda w: self.on_color_property_changed(prop_name, w.get_rgba(), 'color'))
-    #         container.append(color)
-
-    #     elif prop_type == 'combo':
-    #         if prop['options']:
-    #             opt_strs = [opt['label'] for opt in prop['options']]
-    #             dd = Gtk.DropDown.new_from_strings(opt_strs)
-    #             dd.set_hexpand(False)
-    #             dd.set_size_request(280, -1)
-    #             
-    #             idx = 0
-    #             for i, opt in enumerate(prop['options']):
-    #                 if str(opt['value']) == str(current_value):
-    #                     idx = i
-    #                     break
-    #             dd.set_selected(idx)
-    #             dd.connect('notify::selected', lambda w, p: self.on_combo_property_changed(prop_name, prop['options'], w.get_selected(), 'combo'))
-    #             container.append(dd)
-    #         else:
-    #             lbl = Gtk.Label(label="No options")
-    #             lbl.add_css_class("text-muted")
-    #             container.append(lbl)
-
-    #     return container
-
-    # def on_property_changed(self, prop_name: str, value, prop_type: str):
-    #     if not self.selected_wp: return
-    #     if prop_type == 'boolean':
-    #         self.prop_manager.set_user_property(self.selected_wp, prop_name, bool(value))
-    #     elif prop_type == 'slider':
-    #         self.prop_manager.set_user_property(self.selected_wp, prop_name, float(value))
-    #     
-    #     self.check_reapply()
-
-    # def on_color_property_changed(self, prop_name: str, color: Gdk.RGBA, prop_type: str):
-    #     if not self.selected_wp: return
-    #     r, g, b = color.red, color.green, color.blue
-    #     self.prop_manager.set_user_property(self.selected_wp, prop_name, (r, g, b))
-    #     self.check_reapply()
-
-    # def on_combo_property_changed(self, prop_name: str, options: List[Dict], idx: int, prop_type: str):
-    #     if not self.selected_wp: return
-    #     if idx < len(options):
-    #         self.prop_manager.set_user_property(self.selected_wp, prop_name, options[idx]['value'])
-    #         self.check_reapply()
-
-    # def check_reapply(self):
-    #     # We need to know if this wallpaper is currently active.
-    #     # Ideally the parent or app tells us, or we query the config/controller.
-    #     # But controller logic is: "apply(id)".
-    #     # For now, let's assume we re-apply if it matches lastWallpaper in config?
-    #     # Or better: The Sidebar doesn't know about "active" state easily. 
-    #     # But `wallpaper_gui.py` checked `self.active_wp == self.selected_wp`.
-    #     # I'll rely on a callback or simple check.
-    #     # Actually, let's just trigger an event or call controller if it IS the current one.
-    #     # Since I don't have `active_wp` passed in, I'll check config.
-    #     
-    #     last_wp = self.prop_manager._config.get("lastWallpaper")
-    #     if self.selected_wp == last_wp:
-    #         self.log_manager.add_debug(f"Property changed for active wallpaper {self.selected_wp}, reapplying...", "Sidebar")
-    #         self.controller.apply(self.selected_wp)
-
-
-    # def on_stop_clicked(self, btn):
-    #     current_screen = self.get_current_screen()
-    #     if current_screen:
-    #         self.controller.stop_screen(current_screen)
-    #         self.log_manager.add_info(f"Stopped wallpaper on {current_screen}", "Sidebar")
 
     def on_apply_clicked(self, btn):
         if self.selected_wp:
